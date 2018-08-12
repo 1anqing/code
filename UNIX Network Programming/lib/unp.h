4f6e8cb00244c63e2f4afb940334adb072869317
1anqing <li.qinggui@zte.com.cn>
2018-08-12 11:23:02 +0800

    code

---
+++ UNIX Network Programming/lib/unp.h
@@ -0,0 +1,17 @@
+/* Our own header. Tabs are set for 4 spaces, not 8 */
+
+#ifndef __unp_h
+#define __unp_h
+
+#include <sys/socket.h>     /* basic socket definitions */
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define MAXLINE     4096        /* max text line length */
+#define BUFFSIZE    8192        /* buffer size for read and writes */
+
+
+#endif
